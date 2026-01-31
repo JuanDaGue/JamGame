369f5ebd2f60732d828737871378e630ea50e131
oscabaho <osbaho@hotmail.com>
2026-01-31 12:56:05 -0500

    Add scenes & terrain; update minigames, settings
    Add two new scenes (Carnival, Menu) and a new Terrain asset. Introduce HubInteractionInput and Duck Hunter minigame files (with meta entries). Apply code updates to GameConstants, MinigameStand and MinigameController, and tweak DefaultVolumeProfile, GraphicsSettings and TagManager. Update package manifest, add project settings for sequences and scene templates, and include SceneSetupGuide and documentation updates.

--- Assets/Scripts/Core/GameConstants.cs
+++ Assets/Scripts/Core/GameConstants.cs
@@ -3,5 +3,9 @@ namespace GameJam.Core
     public static class GameConstants
     {
         public const string PLAYER_TAG = "Player";
+
+        // Scene Names
+        public const string SCENE_HUB = "Carnival";
+        public const string SCENE_MINIGAME_DUCK = "Duck_Hunter";
     }
 }
